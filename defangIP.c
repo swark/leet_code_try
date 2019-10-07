515b30dc8999a8bed56425a51d1d8c48178a3600
swark <swark0321@gmail.com>
2019-10-08 00:47:28 +0800

    first try

---
+++ defangIP.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+char * defangIPaddr(char * address){
+	int size=strlen(address)+7;
+	char *ans=malloc(sizeof(char)*size);
+	while(address && *address!='\r' && *address!='\n' && *address!='\0'){
+		if(*address=='.'){
+			snprintf(ans+strlen(ans), size-strlen(ans), "%s", "[.]");
+		}else{
+			snprintf(ans+strlen(ans), size-strlen(ans), "%c", *address);
+		}
+		address++;
+	}
+	ans[size]='\0';
+	return ans;
+}
+int main(){
+	char str[17];
+	char *ans;
+	do{
+		printf("#");
+		scanf("%s",str);
+		if(!strcmp(str,"end"))
+			break;
+		ans=defangIPaddr(str);
+		printf("ans=%s\n",ans);
+	}while(0);
+	return 0;
+}
